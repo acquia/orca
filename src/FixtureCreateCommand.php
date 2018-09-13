2ec798fd232715f7a9694d1f97db4fac8ea27ad3
TravisCarden <travis.carden@acquia.com>
2018-09-13 12:34:59 -0500

    Added installation of Acquia product modules to fixture:create command.

--- src/FixtureCreateCommand.php
+++ src/FixtureCreateCommand.php
@@ -25,7 +25,9 @@ class FixtureCreateCommand extends CommandBase {
       $result = $this->collectionBuilder()
         ->addTask($this->createBltProject())
         ->addTask($this->installDrupal())
-        ->addTask($this->commitCodeChanges())
+        ->addTask($this->commitCodeChanges('Installed Drupal.'))
+        ->addTask($this->addAcquiaProductModules())
+        ->addTask($this->commitCodeChanges('Added Acquia product modules.', self::BASE_FIXTURE_BRANCH))
         ->run();
     }
     catch (\Exception $e) {
@@ -48,17 +50,36 @@ class FixtureCreateCommand extends CommandBase {
 
   /**
    * Commits code changes made to the build directory.
+   *
+   * @param string $message
+   *   The commit message to use.
+   * @param string|false $backup_branch
+   *   The name of a branch to point at the commit for backup or FALSE to skip
+   *   creating one.
+   *
+   * @return \Robo\Task\Vcs\GitStack
    */
-  private function commitCodeChanges() {
-    return $this->collectionBuilder()
-      ->addTask(
-        $this->taskGitStack()
-          ->dir(self::BUILD_DIR)
-          ->silent(TRUE)
-          ->add('.')
-          ->commit('Installed Drupal.')
-          ->exec('branch -f ' . self::BASE_FIXTURE_BRANCH)
-      );
+  private function commitCodeChanges($message, $backup_branch = FALSE) {
+    $task = $this->taskGitStack()
+      ->dir(self::BUILD_DIR)
+      ->silent(TRUE)
+      ->add('.')
+      ->commit($message, '--allow-empty');
+    if ($backup_branch) {
+      $task->exec("branch -f {$backup_branch}");
+    }
+    return $task;
+  }
+
+  /**
+   * Adds Acquia product modules to the codebase.
+   *
+   * @return \Robo\Contract\TaskInterface
+   */
+  private function addAcquiaProductModules() {
+    return $this->taskComposerRequire()
+      ->dependency(require __DIR__ . '/../config/product-modules.inc.php')
+      ->dir(self::BUILD_DIR);
   }
 
 }
