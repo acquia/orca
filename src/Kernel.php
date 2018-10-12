6a9272b6ba6f60a74719ff47b21d115292ca0a2d
TravisCarden <travis.carden@acquia.com>
2018-10-12 06:06:04 +0000

    Applied coding standards to kernel.

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,53 +9,67 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-class Kernel extends BaseKernel
-{
-    use MicroKernelTrait;
+/**
+ * Manages an environment made of bundles.
+ */
+class Kernel extends BaseKernel {
 
-    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+  use MicroKernelTrait;
 
-    public function getCacheDir()
-    {
-        return $this->getProjectDir().'/var/cache/'.$this->environment;
+  const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheDir() {
+    return $this->getProjectDir() . '/var/cache/' . $this->environment;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLogDir() {
+    return $this->getProjectDir() . '/var/log';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function registerBundles() {
+    $contents = require $this->getProjectDir() . '/config/bundles.php';
+    foreach ($contents as $class => $envs) {
+      if (isset($envs['all']) || isset($envs[$this->environment])) {
+        yield new $class();
+      }
     }
+  }
 
-    public function getLogDir()
-    {
-        return $this->getProjectDir().'/var/log';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader) {
+    $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
+    // Feel free to remove the "container.autowiring.strict_mode" parameter
+    // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+    $container->setParameter('container.autowiring.strict_mode', TRUE);
+    $container->setParameter('container.dumper.inline_class_loader', TRUE);
+    $confDir = $this->getProjectDir() . '/config';
 
-    public function registerBundles()
-    {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
-        foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
-                yield new $class();
-            }
-        }
-    }
+    $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+    $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+    $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+    $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+  }
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
-    {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        // Feel free to remove the "container.autowiring.strict_mode" parameter
-        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
-        $container->setParameter('container.autowiring.strict_mode', true);
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir().'/config';
+  /**
+   * {@inheritdoc}
+   */
+  protected function configureRoutes(RouteCollectionBuilder $routes) {
+    $confDir = $this->getProjectDir() . '/config';
 
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
-    }
+    $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+    $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+    $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+  }
 
-    protected function configureRoutes(RouteCollectionBuilder $routes)
-    {
-        $confDir = $this->getProjectDir().'/config';
-
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
-    }
 }
