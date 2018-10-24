e291a0050820d3f99c47457fc69d3aed8e7da3fb
Adam <adam@phenaproxima.net>
2018-10-24 17:17:52 -0400

    Configure PHPUnit to run headless Chrome (#5)
    * Configure MINK_DRIVER_ARGS_WEBDRIVER environment variable in phpunit.xml.dist to support headless Chrome.
    
    * Refactored slightly.
    
    * Fixed problem with double encoding when tester is rerun.

--- src/Tests/Tester.php
+++ src/Tests/Tester.php
@@ -87,15 +87,42 @@ class Tester {
     $doc->load($path);
     $xpath = new \DOMXPath($doc);
 
-    // Set Simpletest settings.
+    $this->setSimpletestSettings($path, $doc, $xpath);
+    $this->disableSymfonyDeprecationsHelper($path, $doc, $xpath);
+    $this->setMinkDriverArguments($path, $doc, $xpath);
+  }
+
+  /**
+   * Sets Simpletest settings.
+   *
+   * @param string $path
+   *   The path.
+   * @param \DOMDocument $doc
+   *   The DOM document.
+   * @param \DOMXPath $xpath
+   *   The XPath object.
+   */
+  private function setSimpletestSettings(string $path, \DOMDocument $doc, \DOMXPath $xpath): void {
     $xpath->query('//phpunit/php/env[@name="SIMPLETEST_BASE_URL"]')
       ->item(0)
       ->setAttribute('value', sprintf('http://%s', self::WEB_ADDRESS));
     $xpath->query('//phpunit/php/env[@name="SIMPLETEST_DB"]')
       ->item(0)
       ->setAttribute('value', 'sqlite://localhost/sites/default/files/.ht.sqlite');
+    $doc->save($path);
+  }
 
-    // Disable Symfony deprecations helper.
+  /**
+   * Disables the Symfony Deprecations Helper.
+   *
+   * @param string $path
+   *   The path.
+   * @param \DOMDocument $doc
+   *   The DOM document.
+   * @param \DOMXPath $xpath
+   *   The XPath object.
+   */
+  private function disableSymfonyDeprecationsHelper(string $path, \DOMDocument $doc, \DOMXPath $xpath): void {
     if (!$xpath->query('//phpunit/php/env[@name="SYMFONY_DEPRECATIONS_HELPER"]')->length) {
       $element = $doc->createElement('env');
       $element->setAttribute('name', 'SYMFONY_DEPRECATIONS_HELPER');
@@ -103,9 +130,58 @@ class Tester {
       $xpath->query('//phpunit/php')
         ->item(0)
         ->appendChild($element);
+      $doc->save($path);
+    }
+  }
+
+  /**
+   * Sets the mink driver arguments.
+   *
+   * @param string $path
+   *   The path.
+   * @param \DOMDocument $doc
+   *   The DOM document.
+   * @param \DOMXPath $xpath
+   *   The XPath object.
+   */
+  private function setMinkDriverArguments(string $path, \DOMDocument $doc, \DOMXPath $xpath): void {
+    // Create an <env> element containing a JSON array which will control how
+    // the Mink driver interacts with Chromedriver.
+    $mink_arguments = json_encode([
+      'chrome',
+      [
+        'chrome' => [
+          // Start Chrome in headless mode.
+          'switches' => ['headless', 'disable-gpu'],
+        ],
+      ],
+      'http://localhost:4444',
+    ], JSON_UNESCAPED_SLASHES);
+
+    $expression = '//phpunit/php/env[@name="MINK_DRIVER_ARGS_WEBDRIVER"]';
+
+    if (!$xpath->query($expression)->length) {
+      $element = $doc->createElement('env');
+      $element->setAttribute('name', 'MINK_DRIVER_ARGS_WEBDRIVER');
+      $xpath->query('//phpunit/php')
+        ->item(0)
+        ->appendChild($element);
     }
 
-    $doc->save($path);
+    $xpath->query($expression)
+      ->item(0)
+      ->setAttribute('value', $mink_arguments);
+
+    // When dumping the XML document tree, PHP will encode all double quotes in
+    // the JSON string to &quot;, since the XML attribute value is itself
+    // enclosed in double quotes. There's no way to change this behavior, so
+    // we must do a string replacement in order to wrap the Mink driver
+    // arguments in single quotes.
+    // @see https://stackoverflow.com/questions/5473520/php-dom-and-single-quotes#5473718
+    $search = sprintf('value="%s"', htmlentities($mink_arguments));
+    $replace = sprintf("value='%s'", $mink_arguments);
+    $xml = str_replace($search, $replace, $doc->saveXML());
+    file_put_contents($path, $xml);
   }
 
   /**
