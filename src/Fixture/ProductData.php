1e5138c077a391f631f09b0addc95ec24d518dd6
TravisCarden <travis.carden@acquia.com>
2018-11-11 01:00:57 -0500

    Moved (product) project details into YAML configuration.

--- src/Fixture/ProductData.php
+++ src/Fixture/ProductData.php
@@ -2,6 +2,8 @@
 
 namespace Acquia\Orca\Fixture;
 
+use Symfony\Component\Yaml\Yaml;
+
 /**
  * Provides access to Acquia product module data.
  */
@@ -12,149 +14,18 @@ class ProductData {
    *
    * @var array
    */
-  protected static $data = [
-    'acquia/acsf-tools' => [
-      'version' => '*',
-      'dir' => 'acsf-tools',
-      'module' => FALSE,
-    ],
+  protected $data = [];
 
-    'acquia/drupal-spec-tool' => [
-      'version' => '*',
-      'dir' => 'drupal-spec-tool',
-      'module' => FALSE,
-    ],
-
-    'drupal/acquia_commercemanager' => [
-      'version' => '*',
-      'dir' => 'commerce-manager',
-      'module' => 'acm',
-      'submodules' => [
-        // 'acm_cart',
-        'acm_checkout',
-        // 'acm_customer',
-        'acm_diagnostic',
-        // 'acm_exception',
-        // 'acm_payment',
-        // 'acm_product',
-        // 'acm_promotion',
-        // 'acm_sku',
-        // 'acm_sku_position',
-        // @todo Temporary workaround.
-        'pcb',
-      ],
-    ],
-
-    'drupal/acquia_connector' => [
-      'version' => '*',
-      'dir' => 'acquia-connector',
-      'module' => 'acquia_connector',
-      'submodules' => [
-        // 'acquia_search',
-      ],
-    ],
-
-    'drupal/acquia_contenthub' => [
-      'version' => '~1.0',
-      'dir' => 'content-hub-d8',
-      'module' => 'acquia_contenthub',
-      'submodules' => [
-        'acquia_contenthub_diagnostic',
-        'acquia_contenthub_status',
-        'acquia_contenthub_subscriber',
-      ],
-    ],
-
-    'drupal/acquia_lift' => [
-      'version' => '*',
-      'dir' => 'acquia_lift',
-      'module' => 'acquia_lift',
-      'submodules' => [
-        'acquia_lift_inspector',
-      ],
-    ],
-
-    'drupal/acquia_purge' => [
-      'version' => '*',
-      'dir' => 'acquia_purge',
-      'module' => 'acquia_purge',
-    ],
-
-    'drupal/acsf' => [
-      'version' => '*',
-      'dir' => 'acsf',
-      'module' => 'acsf',
-      'submodules' => [
-        'acsf_duplication',
-        'acsf_sso',
-        'acsf_theme',
-        'acsf_variables',
-      ],
-    ],
-
-    'drupal/lightning_api' => [
-      'version' => '*',
-      'dir' => 'lightning-api',
-      'module' => 'lightning_api',
-    ],
-
-    'drupal/lightning_core' => [
-      'version' => '*',
-      'dir' => 'lightning-core',
-      'module' => 'lightning_core',
-      'submodules' => [
-        'lightning_contact_form',
-        'lightning_page',
-        'lightning_roles',
-        'lightning_search',
-      ],
-    ],
-
-    'drupal/lightning_layout' => [
-      'version' => '*',
-      'dir' => 'lightning-layout',
-      'module' => 'lightning_layout',
-      'submodules' => [
-        'lightning_landing_page',
-      ],
-    ],
-
-    'drupal/lightning_media' => [
-      'version' => '*',
-      'dir' => 'lightning-media',
-      'module' => 'lightning_media',
-      'submodules' => [
-        'lightning_media_audio',
-        'lightning_media_bulk_upload',
-        'lightning_media_document',
-        'lightning_media_image',
-        'lightning_media_instagram',
-        'lightning_media_slideshow',
-        'lightning_media_twitter',
-        'lightning_media_video',
-      ],
-    ],
-
-    'drupal/lightning_workflow' => [
-      'version' => '*',
-      'dir' => 'lightning-workflow',
-      'module' => 'lightning_workflow',
-      'submodules' => [
-        'lightning_scheduler',
-      ],
-    ],
-
-    'drupal/media_acquiadam' => [
-      'version' => '*',
-      'dir' => 'media_acquiadam',
-      // 'module' => 'media_acquiadam',
-      'submodules' => [
-        'lightning_acquiadam',
-        // 'media_acquiadam_example',
-        'media_acquiadam_report',
-      ],
-    ],
-  ];
+  /**
+   * Constructs an instance.
+   *
+   * @param string $project_dir
+   *   The ORCA project directory.
+   */
+  public function __construct(string $project_dir) {
+    $yaml = file_get_contents("{$project_dir}/config/projects.yml");
+    $this->data = Yaml::parse($yaml);
+  }
 
   /**
    * Returns the directory name for the given package.
@@ -165,13 +36,13 @@ class ProductData {
    * @return string
    */
   public function dir($package) {
-    if (!array_key_exists($package, self::$data)) {
+    if (!array_key_exists($package, $this->data)) {
       throw new \InvalidArgumentException(
         sprintf('No such package: "%s"', $package)
       );
     }
 
-    return self::$data[$package]['dir'];
+    return $this->data[$package]['dir'];
   }
 
   /**
@@ -196,11 +67,11 @@ class ProductData {
    *   A module name, if available, or FALSE if not.
    */
   public function moduleName($package) {
-    if (empty(self::$data[$package]['module'])) {
+    if (empty($this->data[$package]['module'])) {
       return FALSE;
     }
 
-    return self::$data[$package]['module'];
+    return $this->data[$package]['module'];
   }
 
   /**
@@ -213,7 +84,7 @@ class ProductData {
    */
   public function moduleNamePlural(?string $package = NULL) {
     $modules = [];
-    foreach (self::$data as $package_name => $data) {
+    foreach ($this->data as $package_name => $data) {
       if ($package && $package !== $package_name) {
         continue;
       }
@@ -236,7 +107,7 @@ class ProductData {
    * @return string[]
    */
   public function packageNames() {
-    return array_keys(self::$data);
+    return array_keys($this->data);
   }
 
   /**
@@ -246,7 +117,7 @@ class ProductData {
    */
   public function packageStringPlural() {
     $packages = [];
-    foreach (self::$data as $package_name => $datum) {
+    foreach ($this->data as $package_name => $datum) {
       if (!empty($datum['version'])) {
         $packages[$package_name] = "{$package_name}:{$datum['version']}";
         if (!empty($datum['submodules'])) {
@@ -285,7 +156,7 @@ class ProductData {
    */
   public function projectNamePlural(?string $package = NULL) {
     $names = [];
-    $data = ($package) ? [$package => []] : self::$data;
+    $data = ($package) ? [$package => []] : $this->data;
     foreach (array_keys($data) as $package_name) {
       $names[] = $this->projectName($package_name);
     }
