873bc5a9c1542be84776654ed29554a197903c90
user.email <travis.carden@acquia.com>
2019-04-04 13:32:14 -0400

    Added documentation to bin/pipelines/refresh.sh. [skip ci]

--- bin/pipelines/refresh.sh
+++ bin/pipelines/refresh.sh
@@ -4,11 +4,24 @@
 #     refresh.sh - Refreshes the Cloud environment.
 #
 # SYNOPSIS
-#     refresh.sh
+#     refresh.sh <AH_SITE_GROUP> <AH_SITE_ENVIRONMENT>
 #
 # DESCRIPTION
 #     Refreshes the Acquia Cloud environment by reinstalling Drupal and enabling
-#     all Acquia modules.
+#     all Acquia modules. This script is copied the Cloud Hooks directory on
+#     Acquia Pipelines builds (see acquia-pipelines.yml) and executed via cron:
+#
+#     /var/www/html/${AH_SITE_NAME}/hooks/common/post-code-deploy/refresh.sh \
+#         ${AH_SITE_GROUP} ${AH_SITE_ENVIRONMENT} &>> \
+#         /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/drush-cron.log
+#
+# OPTIONS
+#     <AH_SITE_GROUP>
+#         The site group as stored in the AH_SITE_GROUP environment variable,
+#         e.g., "orca".
+#     <AH_SITE_ENVIRONMENT>
+#         The site environment as stored in the AH_SITE_ENVIRONMENT environment
+#         variable, e.g., "dev" or "prod".
 
 SITE="$1"
 TARGET_ENV="$2"
