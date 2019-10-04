d02e01141c35d6ebfdd8a2b5f0a5d8486080013b
TravisCarden <travis.carden@acquia.com>
2019-10-04 13:15:27 -0400

    Added upstream ORCA status to end of Travis CI output.

--- bin/travis/after_script.sh
+++ bin/travis/after_script.sh
@@ -7,10 +7,16 @@
 #     after_script.sh
 #
 # DESCRIPTION
-#     Logs the job on cron if telemetry is enabled.
+#     Conditionally logs the job and displays upstream ORCA status.
 
 cd "$(dirname "$0")" || exit; source _includes.sh
 
+# Log the job on cron if telemetry is enabled.
 if [[ "$TRAVIS_EVENT_TYPE" = "cron" && "$ORCA_TELEMETRY_ENABLE" && "$ORCA_AMPLITUDE_API_KEY" && "$ORCA_AMPLITUDE_USER_ID" ]]; then
   orca internal:log-job
 fi
+
+# Show ORCA's own current build status. A failure may signify an upstream issue
+# or service level outage that could have affected this build.
+# @see https://travis-ci.org/acquia/orca/branches
+travis history --no-interactive --repo=acquia/orca --branch=master --limit=1 --date
