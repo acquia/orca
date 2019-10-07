fed133773dfa98cfab96c12ab747d530395c1ba1
Travis Carden <TravisCarden@users.noreply.github.com>
2019-10-07 09:09:31 -0400

    Stopped running non-SUT Behat tests (#45)

--- bin/travis/script.sh
+++ bin/travis/script.sh
@@ -25,10 +25,22 @@ case "$ORCA_JOB" in
   "DEPRECATED_CODE_SCAN") eval "orca qa:deprecated-code-scan --sut=$ORCA_SUT_NAME" ;;
   "DEPRECATED_CODE_SCAN_CONTRIB") eval "orca qa:deprecated-code-scan --contrib" ;;
   "ISOLATED_RECOMMENDED") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME --sut-only" ;;
-  "INTEGRATED_RECOMMENDED") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY" ;;
-  "CORE_PREVIOUS") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY" ;;
+  "INTEGRATED_RECOMMENDED")
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY --phpunit"
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME --sut-only --behat"
+    ;;
+  "CORE_PREVIOUS")
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY --phpunit"
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME --sut-only --behat"
+    ;;
   "ISOLATED_DEV") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME --sut-only" ;;
-  "INTEGRATED_DEV") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY" ;;
-  "CORE_NEXT") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY" ;;
+  "INTEGRATED_DEV")
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY --phpunit"
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME --sut-only --behat"
+    ;;
+  "CORE_NEXT")
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME $SUT_ONLY --phpunit"
+    eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME --sut-only --behat"
+    ;;
   "CUSTOM") eval "orca qa:automated-tests --sut=$ORCA_SUT_NAME ${ORCA_CUSTOM_TESTS_RUN_ARGS:=}" ;;
 esac
