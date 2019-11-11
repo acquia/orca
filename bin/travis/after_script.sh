36e5908dc89a002d8501bd94d5f8b9559d5ec8ff
TravisCarden <travis.carden@acquia.com>
2019-11-11 12:26:08 -0500

    Added a little whitespace to make ORCA build status more readable.

--- bin/travis/after_script.sh
+++ bin/travis/after_script.sh
@@ -20,4 +20,4 @@ fi
 # Show ORCA's own current build status. A failure may signify an upstream issue
 # or service level outage that could have affected this build.
 # @see https://travis-ci.org/acquia/orca/branches
-travis history --no-interactive --repo=acquia/orca --branch=master --limit=2 --date
+echo && travis history --no-interactive --repo=acquia/orca --branch=master --limit=2 --date
