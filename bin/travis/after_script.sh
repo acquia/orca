94b1c3c1c2494d08374d5e6a8b580fcee7b66f8a
TravisCarden <travis.carden@acquia.com>
2019-11-11 12:24:53 -0500

    Output telemetry data that gets sent after script.

--- bin/travis/after_script.sh
+++ bin/travis/after_script.sh
@@ -14,6 +14,7 @@ cd "$(dirname "$0")" || exit; source _includes.sh
 # Log the job on cron if telemetry is enabled.
 if [[ "$TRAVIS_EVENT_TYPE" = "cron" && "$ORCA_TELEMETRY_ENABLE" && "$ORCA_AMPLITUDE_API_KEY" && "$ORCA_AMPLITUDE_USER_ID" ]]; then
   orca internal:log-job
+  orca internal:log-job --simulate
 fi
 
 # Show ORCA's own current build status. A failure may signify an upstream issue
