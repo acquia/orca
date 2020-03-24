59628135e43a24a5ae59ce5dbf9d013d53967bdb
TravisCarden <travis.carden@acquia.com>
2020-03-24 15:52:08 -0400

    Added integrated live test job for testing with default packages.

---
+++ bin/travis/self-test/_includes.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# NAME
+#     _includes.sh - Include reusable code.
+#
+# SYNOPSIS
+#     cd "$(dirname "$0")" || exit; source _includes.sh
+#
+# DESCRIPTION
+#     Includes common features used by the Travis CI scripts.
+
+source ../_includes.sh
+
+if [[ "$ORCA_JOB" = "LIVE_TEST" ]]; then
+  unset ORCA_PACKAGES_CONFIG
+  unset ORCA_PACKAGES_CONFIG_ALTER
+fi
