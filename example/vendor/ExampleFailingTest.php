f7a200a5047bb716c30db1ef8864a1c0cc762df1
Dane Powell <git@danepowell.com>
2020-04-14 22:52:50 -0700

    Fixes #73: Don't include SUT vendor directory in tests. (#75)

---
+++ example/vendor/ExampleFailingTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\example\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Vendor packages (installed via a SUT) often provide tests that should not be
+ * run by ORCA. This failing test is used to verify that ORCA excludes the
+ * vendor directory as expected.
+ */
+class ExampleFailingTest extends TestCase {
+
+  /**
+   * ORCA should never run this test.
+   */
+  public function testExcludeVendorDirectory() {
+    $this->assertTrue(FALSE);
+  }
+
+}
