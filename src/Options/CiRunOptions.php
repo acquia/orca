80b537ce8ab3070adaf63c6282f3089bb556c4b7
TravisCarden <travis.carden@acquia.com>
2020-09-14 17:45:56 -0400

    Added new CiJobPhaseOptions class.

---
+++ src/Options/CiRunOptions.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Acquia\Orca\Options;
+
+use Acquia\Orca\Enum\CiJobEnum;
+use Acquia\Orca\Enum\CiJobPhaseEnum;
+use Acquia\Orca\Exception\OrcaInvalidArgumentException;
+use Closure;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use UnexpectedValueException;
+
+/**
+ * Encapsulates CI run options.
+ */
+class CiRunOptions {
+
+  /**
+   * The resolved options.
+   *
+   * @var array
+   */
+  private $options;
+
+  /**
+   * Constructs an instance.
+   *
+   * @param array $options
+   *   The options.
+   *
+   * @throws \Acquia\Orca\Exception\OrcaInvalidArgumentException
+   */
+  public function __construct(array $options) {
+    $this->resolve($options);
+  }
+
+  /**
+   * Resolves the given options.
+   *
+   * @param array $options
+   *   The options to resolve.
+   *
+   * @throws \Acquia\Orca\Exception\OrcaInvalidArgumentException
+   */
+  private function resolve(array $options): void {
+    try {
+      $this->options = (new OptionsResolver())
+        // Set defined.
+        ->setDefined([
+          'job',
+          'phase',
+        ])
+        // Set required.
+        ->setRequired([
+          'job',
+          'phase',
+        ])
+        // Set allowed types.
+        ->setAllowedTypes('job', 'string')
+        ->setAllowedTypes('phase', 'string')
+        // Set allowed values.
+        ->setAllowedValues('job', $this->isValidJobValue())
+        ->setAllowedValues('phase', $this->isValidPhaseValue())
+        // Resolve.
+        ->resolve($options);
+    }
+    catch (UndefinedOptionsException | InvalidOptionsException $e) {
+      throw new OrcaInvalidArgumentException($e->getMessage());
+    }
+  }
+
+  /**
+   * Validates the "job" value.
+   *
+   * @return \Closure
+   *   The validation function.
+   */
+  private function isValidJobValue(): Closure {
+    return static function ($value): bool {
+      try {
+        new CiJobEnum($value);
+      }
+      catch (UnexpectedValueException $e) {
+        return FALSE;
+      }
+      return TRUE;
+    };
+  }
+
+  /**
+   * Validates the "job" value.
+   *
+   * @return \Closure
+   *   The validation function.
+   */
+  private function isValidPhaseValue(): Closure {
+    return static function ($value): bool {
+      try {
+        new CiJobPhaseEnum($value);
+      }
+      catch (UnexpectedValueException $e) {
+        return FALSE;
+      }
+      return TRUE;
+    };
+  }
+
+  /**
+   * Gets the job name.
+   *
+   * @return string
+   *   The job name.
+   */
+  public function getJob(): string {
+    return $this->options['job'];
+  }
+
+  /**
+   * Gets the phase name.
+   *
+   * @return string
+   *   The phase name.
+   */
+  public function getPhase(): string {
+    return $this->options['phase'];
+  }
+
+}
---
+++ tests/Options/CiRunOptionsTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Acquia\Orca\Tests\Options;
+
+use Acquia\Orca\Options\CiRunOptions;
+use Acquia\Orca\Enum\CiJobEnum;
+use Acquia\Orca\Enum\CiJobPhaseEnum;
+use Acquia\Orca\Exception\OrcaInvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
+
+/**
+ * @coversDefaultClass \Acquia\Orca\Options\CiRunOptions
+ */
+class CiRunOptionsTest extends TestCase {
+
+  /**
+   * @dataProvider providerJobs
+   *
+   * @covers ::__construct
+   * @covers ::getJob
+   * @covers ::isValidJobValue
+   * @covers ::resolve
+   */
+  public function testValidJobs($job): void {
+    $options = new CiRunOptions([
+      'job' => $job,
+      'phase' => $this->validPhase(),
+    ]);
+
+    self::assertSame($job, $options->getJob(), 'Set/got "job" option.');
+  }
+
+  /**
+   * @dataProvider providerPhases
+   *
+   * @covers ::__construct
+   * @covers ::getPhase
+   * @covers ::isValidPhaseValue
+   * @covers ::resolve
+   */
+  public function testValidPhases($phase): void {
+    $options = new CiRunOptions([
+      'job' => $this->validJob(),
+      'phase' => $phase,
+    ]);
+
+    self::assertSame($phase, $options->getPhase(), 'Set/got "phase" phase.');
+  }
+
+  /**
+   * @dataProvider providerMissingRequiredOptions
+   */
+  public function testMissingRequiredOptions($options): void {
+    $this->expectException(MissingOptionsException::class);
+
+    new CiRunOptions($options);
+  }
+
+  public function providerMissingRequiredOptions(): array {
+    return [
+      [[]],
+      [['phase' => $this->validPhase()]],
+      [['job' => $this->validJob()]],
+    ];
+  }
+
+  /**
+   * @covers ::resolve
+   */
+  public function testUndefinedOptions(): void {
+    $this->expectException(OrcaInvalidArgumentException::class);
+
+    new CiRunOptions([
+      'job' => $this->validJob(),
+      'phase' => $this->validPhase(),
+      'undefined' => 'option',
+    ]);
+  }
+
+  /**
+   * @dataProvider providerInvalidOptions
+   *
+   * @covers ::resolve
+   * @covers ::isValidJobValue
+   * @covers ::isValidPhaseValue
+   */
+  public function testInvalidOptions($options): void {
+    $this->expectException(OrcaInvalidArgumentException::class);
+
+    new CiRunOptions($options);
+  }
+
+  public function providerInvalidOptions(): array {
+    return [
+      [['job' => $this->validJob(), 'phase' => 12345]],
+      [['job' => 12345, 'phase' => $this->validPhase()]],
+      [['job' => $this->validJob(), 'phase' => 'invalid']],
+      [['job' => 'invalid', 'phase' => $this->validPhase()]],
+    ];
+  }
+
+  public function providerJobs(): array {
+    $jobs = CiJobEnum::keys();
+    array_walk($jobs, static function (&$value) {
+      $value = [$value];
+    });
+    return $jobs;
+  }
+
+  public function providerPhases(): array {
+    $phases = CiJobPhaseEnum::keys();
+    array_walk($phases, static function (&$value) {
+      $value = [strtolower($value)];
+    });
+    return $phases;
+  }
+
+  private function validJob(): string {
+    $jobs = CiJobEnum::keys();
+    return reset($jobs);
+  }
+
+  private function validPhase(): string {
+    $phases = CiJobPhaseEnum::keys();
+    return strtolower(reset($phases));
+  }
+
+}
