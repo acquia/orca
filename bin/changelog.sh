15f6db5b375acf8610a2a4b710cf7c8ce0080039
TravisCarden <travis.carden@acquia.com>
2020-11-09 14:29:26 -0500

    Added changelog script. [skip ci]

---
+++ bin/changelog.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# NAME
+#     changelog.sh - Generate a changelog.
+#
+# SYNOPSIS
+#     changelog.sh
+#
+# DESCRIPTION
+#     Generates a changelog using the GitHub Changelog Generator:
+#     https://github.com/github-changelog-generator/github-changelog-generator
+#     Only tested on macOS.
+
+cd "$(dirname "$0")/.." || exit
+
+GITHUB_CHANGELOG_GENERATOR=$(find /usr/local/lib/ruby/gems/*/bin/github_changelog_generator | tail -1)
+FUTURE_RELEASE=$(cat config/VERSION)
+
+eval "$GITHUB_CHANGELOG_GENERATOR" \
+  --user=acquia \
+  --project=orca \
+  --output=docs/CHANGELOG.md \
+  --no-issues-wo-labels \
+  --no-issues-wo-labels \
+  --unreleased-only \
+  --exclude-labels=dependencies,duplicate,question,invalid,wontfix \
+  --since-tag=v2.0.0 \
+  --future-release="$FUTURE_RELEASE" \
+  --release-branch=develop
