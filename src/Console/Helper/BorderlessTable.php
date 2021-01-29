753b387584735e50d30dc187af4e86c0afe979b9
Travis Carden <travis.carden@acquia.com>
2021-01-29 00:59:33 -0500

    Changed "magic number" to "health score" and updated logic to account for config files. (#128)

---
+++ src/Console/Helper/BorderlessTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Acquia\Orca\Console\Helper;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableStyle;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Provides a table for status displays.
+ */
+class BorderlessTable extends Table {
+
+  /**
+   * Constructs an instance.
+   *
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   *   The output decorator.
+   */
+  public function __construct(OutputInterface $output) {
+    parent::__construct($output);
+    $this->setStyle($this->tableStyle());
+  }
+
+  /**
+   * Provides the table style.
+   *
+   * @return \Symfony\Component\Console\Helper\TableStyle
+   *   A TableStyle object.
+   */
+  private function tableStyle(): TableStyle {
+    return (new TableStyle())
+      ->setHorizontalBorderChars('', '')
+      ->setVerticalBorderChars('', '')
+      ->setDefaultCrossingChar('');
+  }
+
+}
