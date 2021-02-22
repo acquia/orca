7d7ce46989ed6954be32105ffb11d3b14e2734a4
Dane Powell <git@danepowell.com>
2021-02-22 06:12:55 -0800

    Fix command not found error in before_cache.sh (#130)

--- bin/travis/before_cache.sh
+++ bin/travis/before_cache.sh
@@ -9,6 +9,8 @@
 # DESCRIPTION
 #     Reserved for future use.
 
+cd "$(dirname "$0")" || exit; source _includes.sh
+
 if [[ "$ORCA_JOB" ]]; then
   eval "orca ci:run $ORCA_JOB before_cache $ORCA_SUT_NAME"
 fi
