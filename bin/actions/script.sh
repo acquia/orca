917f2ce20c96debec3092ffff9f0f27423f70345
Dane Powell <git@danepowell.com>
2021-08-27 08:52:25 -0700

    ORCA-189: Support Github Actions

---
+++ bin/actions/script.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# NAME
+#     script.sh - Run ORCA tests.
+#
+# SYNOPSIS
+#     script.sh
+#
+# DESCRIPTION
+#     Runs static code analysis and automated tests.
+
+cd "$(dirname "$0")" || exit; source _includes.sh
+
+if [[ "$ORCA_JOB" ]]; then
+  eval "orca ci:run $ORCA_JOB script $ORCA_SUT_NAME"
+fi
+
+if [[ "$ORCA_ENABLE_NIGHTWATCH" == "TRUE" && "$ORCA_SUT_HAS_NIGHTWATCH_TESTS" && -d "$ORCA_YARN_DIR" ]]; then
+  (
+    cd "$ORCA_YARN_DIR" || exit
+    orca fixture:run-server &
+    PID=$!
+
+    eval "yarn test:nightwatch \\
+      --headless \\
+      --passWithNoTests \\
+      --tag=$ORCA_SUT_MACHINE_NAME"
+
+    kill -0 $PID
+  )
+fi
---
+++ bin/actions/self-test/_includes.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# NAME
+#     _includes.sh - Include reusable code.
+#
+# SYNOPSIS
+#     cd "$(dirname "$0")" || exit; source _includes.sh
+#
+# DESCRIPTION
+#     Includes common features used by the Github Actions scripts.
+
+source ../_includes.sh
+
+if [[ "$ORCA_LIVE_TEST" ]]; then
+  unset ORCA_PACKAGES_CONFIG
+  unset ORCA_PACKAGES_CONFIG_ALTER
+fi
