36896cbaff8c61c7a211869ad1106cac78dd4e2d
Dane Powell <git@danepowell.com>
2021-09-02 12:20:43 -0700

    noservers

--- bin/actions/script.sh
+++ bin/actions/script.sh
@@ -12,6 +12,6 @@
 cd "$(dirname "$0")" || exit; source _includes.sh
 
 if [[ "$ORCA_JOB" ]]; then
-  eval "orca qa:automated-tests --sut=acquia/blt --sut-only -vvv"
+  eval "orca qa:automated-tests --sut=acquia/blt --sut-only --no-servers -vvv"
 fi
 
