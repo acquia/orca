099fe6781598b72400544e67d30ab22f57b6ec9a
Dane Powell <git@danepowell.com>
2021-09-02 12:19:42 -0700

    debug

--- bin/actions/script.sh
+++ bin/actions/script.sh
@@ -12,6 +12,6 @@
 cd "$(dirname "$0")" || exit; source _includes.sh
 
 if [[ "$ORCA_JOB" ]]; then
-  eval "orca ci:run $ORCA_JOB script $ORCA_SUT_NAME -vvv"
+  eval "orca qa:automated-tests --sut=acquia/blt --sut-only -vvv"
 fi
 
