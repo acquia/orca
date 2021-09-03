2ff8022c4fdeb3402d819a30654d40e18a4f717a
Dane Powell <git@danepowell.com>
2021-09-03 09:53:24 -0700

    Revert "mustrun getexitcode"
    This reverts commit 139847e4bc5fc5235ac606fa8c5b974d229235cc.

--- src/Helper/Process/ProcessRunner.php
+++ src/Helper/Process/ProcessRunner.php
@@ -74,10 +74,10 @@ class ProcessRunner {
       $process->setWorkingDirectory($cwd);
     }
 
-    $process
+    $status = $process
       ->setTimeout(NULL)
       ->setIdleTimeout(NULL)
-      ->mustRun(function () {
+      ->run(function () {
         // Write process buffer to output.
         $buffer = func_get_arg(1);
         $this->output->write($buffer);
@@ -90,7 +90,7 @@ class ProcessRunner {
 
     $this->output->newLine();
 
-    return $process->getExitCode();
+    return $status;
   }
 
   /**
