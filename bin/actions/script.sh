c56351e6ce3c50caa54b308912c67947a5197dd8
Dane Powell <git@danepowell.com>
2021-09-03 08:26:04 -0700

    Revert "debug"
    This reverts commit 099fe6781598b72400544e67d30ab22f57b6ec9a.

--- bin/actions/script.sh
+++ bin/actions/script.sh
@@ -12,6 +12,6 @@
 cd "$(dirname "$0")" || exit; source _includes.sh
 
 if [[ "$ORCA_JOB" ]]; then
-  eval "orca qa:automated-tests --sut=acquia/blt --sut-only -vvv"
+  eval "orca ci:run $ORCA_JOB script $ORCA_SUT_NAME -vvv"
 fi
 
