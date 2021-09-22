3361f97cdca188ad7ef20bfa760dea71ca684930
Dane Powell <git@danepowell.com>
2021-09-22 10:48:25 -0700

    only run after_failure if ORCA_JOB is set

--- bin/ci/after_failure.sh
+++ bin/ci/after_failure.sh
@@ -15,4 +15,6 @@ if [[ -f "$ORCA_FIXTURE_DIR/vendor/bin/drush" && "$(drush core-status --field=bo
   drush watchdog:show --count=100 --severity=Error --extended
 fi
 
-eval "orca ci:run $ORCA_JOB after_failure $ORCA_SUT_NAME"
+if [[ "$ORCA_JOB" ]]; then
+  eval "orca ci:run $ORCA_JOB after_failure $ORCA_SUT_NAME"
+fi
