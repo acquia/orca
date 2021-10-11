0b80bc3df2166b2d497faa49ddae63df584fb83f
Dane Powell <git@danepowell.com>
2021-10-11 15:57:08 -0700

    with-all-dependencies

--- src/Domain/Composer/ComposerFacade.php
+++ src/Domain/Composer/ComposerFacade.php
@@ -238,6 +238,7 @@ class ComposerFacade {
       $command[] = '--no-update';
     }
     $command[] = '--no-interaction';
+    $command[] = '--with-all-dependencies';
 
     $this->runComposer($command, $packages);
   }
