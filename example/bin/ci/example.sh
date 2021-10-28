faf7da56eeac2783d0d3d0c11a7c3f480e2a4fb3
Dane Powell <git@danepowell.com>
2021-10-28 11:35:57 -0700

    Add GitHub Actions custom script docs (#167)
    Co-authored-by: Travis Carden <travis.carden@gmail.com>

--- example/bin/ci/example.sh
+++ example/bin/ci/example.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
 
 # NAME
-#     example.sh - Provide an example custom GitHub Actions script.
+#     example.sh - Provide an example custom ORCA script.
 #
 # SYNOPSIS
 #     example.sh
 #
 # DESCRIPTION
-#     Provides an example of customizing a GitHub Actions ORCA build by adding or
+#     Provides an example of customizing an ORCA build by adding or
 #     modifying jobs. See live working examples:
-#     - https://github.com/acquia/coding-standards-php/tree/v0.5.0/bin/actions
-#     - https://github.com/acquia/drupal-spec-tool/tree/4.0.1/bin/actions
-#     - https://github.com/acquia/orca/tree/v2.11.3/bin/actions/self-test
+#     - https://github.com/acquia/coding-standards-php/tree/v0.5.0/bin/travis
+#     - https://github.com/acquia/drupal-spec-tool/tree/4.0.1/bin/travis
+#     - https://github.com/acquia/orca/tree/v2.11.3/bin/travis/self-test
 #
 #     Remember to make your script executable! E.g.:
-#     chmod u+x bin/actions/example.sh
+#     chmod u+x bin/ci/example.sh
 
 # Make bash resolve paths from the location of this script, not the CWD (current
 # working directory) of the user or process that called it.
 cd "$(dirname "$0")" || exit 1
 
 # Reuse ORCA's own includes for its $PATH additions and environment variables.
-source ../../../orca/bin/actions/_includes.sh
+source ../../../orca/bin/ci/_includes.sh
 
-# GitHub Actions provides numerous general purpose environment variables you can use.
-# @see https://docs.github.com/en/actions/reference/environment-variables#default-environment-variables
-echo "The SUT is cloned at $GITHUB_WORKSPACE"
+# ORCA provides numerous general purpose environment variables you can use.
+echo "The SUT is cloned at $CI_WORKSPACE"
 
 # ORCA provides additional special purpose environment variables. Use the below
 # command to see the list with current values. Its output is included in the
-# before_install phase of all GitHub Actions jobs for debugging purposes.
+# before_install phase of all CI jobs for debugging purposes.
 orca debug:env-vars
 
 # Unconditioned statements will be executed on every job.
@@ -46,8 +45,15 @@ if [[ "$ORCA_JOB" == "ISOLATED_TEST_ON_CURRENT" ]]; then
   fi
 fi
 
+# Rather than targeting an out-of-the-box ORCA job, run a completely custom job.
+# ORCA will not run unless ORCA_JOB is set.
+if [[ ! "$ORCA_JOB" ]]; then
+  # Do something totally custom as your own job.
+  composer install
+fi
+
 # Target any condition of your own making, e.g., an environment variable set in
-# your orca.yml.
+# your CI script.
 if [[ "$MY_CONDITION" == "TRUE" ]]; then
   echo "\$MY_CONDITION attained"
 fi
