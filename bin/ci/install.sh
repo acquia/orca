4ce256b27a737a5d0c69dcde7d0dbf0eefed24b7
Katherine Druckman <katherine.druckman@acquia.com>
2021-10-29 16:57:25 -0500

    Add php8 switch and workspace var.

--- bin/ci/install.sh
+++ bin/ci/install.sh
@@ -11,6 +11,12 @@
 
 cd "$(dirname "$0")" || exit; source _includes.sh
 
+# Switch to PHP8.
+if [[ "$PHP8_JOB" == TRUE && "$JENKINS_HOME" ]]; then
+  update-alternatives --install /usr/local/bin/php php /usr/bin/php8.0 80
+  php -v
+fi
+
 if [[ "$ORCA_JOB" ]]; then
   eval "orca ci:run $ORCA_JOB install $ORCA_SUT_NAME"
 fi
