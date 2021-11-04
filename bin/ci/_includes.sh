4060c9114737187b672fa929c9ba1e283cddd2a8
Dane Powell <git@danepowell.com>
2021-11-04 13:24:54 -0700

    clarify comment

--- bin/ci/_includes.sh
+++ bin/ci/_includes.sh
@@ -94,12 +94,12 @@ export PATH="$HOME/.phpenv/shims/:$PATH"
 # Add convenient aliases.
 alias drush='drush -r "$ORCA_FIXTURE_DIR"'
 
-# Commands exiting with a non-zero status prior to this point should always be
-# considered an 'error', i.e. an ORCA configuration problem, and fail the job.
+# Commands exiting with a non-zero status prior to this point constitute an
+# error, i.e. an ORCA configuration problem, and always stop execution.
 
-# Commands exiting with a non-zero status after this point are considered a
-# 'failure', i.e. a problem with the SUT or test fixture. Some jobs are allowed
-# to fail and will still report as a pass.
+# Commands exiting with a non-zero status after this point constitute a failure,
+# i.e. a problem with the SUT or test fixture, and may or may not stop execution
+# depending on whether the job is allowed to fail.
 allowed_failures=(
   "INTEGRATED_TEST_ON_NEXT_MINOR"
   "INTEGRATED_TEST_ON_NEXT_MINOR_DEV"
