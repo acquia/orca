bb7bf8b66396cd2630af48b7ed16aef9c90dff14
sayan goswami <sayan.goswami@acquia.com>
2023-03-13 16:31:21 +0530

    Fix packages alter issues

--- src/Domain/Package/PackageManager.php
+++ src/Domain/Package/PackageManager.php
@@ -181,7 +181,7 @@ class PackageManager {
       // Skipping a null datum provides for a package to be effectively removed
       // from the active specification at runtime by setting its value to NULL
       // in the packages configuration alter file.
-      if ($datum === NULL) {
+      if ($datum === NULL || (array_key_exists('core_matrix', $datum) && $datum['core_matrix'] === NULL)) {
         continue;
       }
 
@@ -190,12 +190,7 @@ class PackageManager {
         continue;
       }
 
-      if ((array_key_exists('version', $datum) || array_key_exists('version_dev', $datum)) && $this->containsValidVersion($datum)) {
-        $this->addPackage($datum, $fixture_path_handler, $package_name);
-        continue;
-      }
-
-      if (isset($datum['core_matrix'])) {
+      if (array_key_exists('core_matrix', $datum)) {
         $constraints = array_values($datum['core_matrix']);
         foreach ($constraints as $constraint) {
           if ($this->containsValidVersion($constraint)) {
@@ -203,6 +198,12 @@ class PackageManager {
             break;
           }
         }
+        continue;
+      }
+
+      if (is_array($datum) && $this->containsValidVersion($datum)) {
+        $this->addPackage($datum, $fixture_path_handler, $package_name);
+        continue;
       }
 
     }
@@ -234,6 +235,11 @@ class PackageManager {
    * Checks if a package is null.
    */
   private function containsValidVersion(array $data) : bool {
+
+    if (!array_key_exists('version', $data) && !array_key_exists('version_dev', $data)) {
+      return TRUE;
+    }
+
     return (array_key_exists('version', $data) && !is_null($data['version'])) || (array_key_exists('version_dev', $data) && !is_null($data['version_dev']));
   }
 
