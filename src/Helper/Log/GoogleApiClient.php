7a509c98c2d985de8d9cb76aabbbd36c0b2706ef
sayan goswami <sayan.goswami@acquia.com>
2023-11-08 15:17:11 +0530

    add sheet2

--- src/Helper/Log/GoogleApiClient.php
+++ src/Helper/Log/GoogleApiClient.php
@@ -64,7 +64,7 @@ class GoogleApiClient {
   /**
    * The sheet id of the spreadsheet.
    */
-  private const SHEET_ID = "Sheet1";
+  private const SHEET_ID = "Sheet2";
 
   /**
    * Constructs an instance.
