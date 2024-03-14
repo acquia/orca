2a3d8d5b54176f1d72cb8dca143cb6319d883e0a
sayan goswami <sayan.goswami@acquia.com>
2024-03-14 09:51:53 +0530

    Use DRS and test

--- src/Options/FixtureOptions.php
+++ src/Options/FixtureOptions.php
@@ -304,10 +304,10 @@ class FixtureOptions {
       $this->options['project-template'] = 'acquia/drupal-recommended-project:^1';
     }
     elseif ($this->coreVersionParsedMatches('^10') && $this->isDev()) {
-      $this->options['project-template'] = 'acquia/drupal-recommended-project:dev-master';
+      $this->options['project-template'] = 'acquia/drupal-recommended-project:dev-ACMS-1866';
     }
     else {
-      $this->options['project-template'] = 'acquia/drupal-recommended-project';
+      $this->options['project-template'] = 'acquia/drupal-recommended-project:dev-ACMS-1866';
     }
 
     return $this->options['project-template'];
