713cbe8121cca03531a1e26b2b0df985813e2615
sayan goswami <sayan.goswami@acquia.com>
2024-07-29 12:52:43 +0530

    conditionally add variable

--- src/Domain/Tool/Phpunit/PhpUnitTask.php
+++ src/Domain/Tool/Phpunit/PhpUnitTask.php
@@ -419,9 +419,12 @@ class PhpUnitTask extends TestFrameworkBase {
         $this->processRunner->addEnvVar("XDEBUG_MODE", "coverage");
       }
 
+      if ($this->envFacade->get('ORCA_PHPUNIT_DEBUG_MODE') === 'true') {
+        $command[] = '--debug';
+      }
+
       $command = array_merge($command, [
         '--colors=always',
-        '--debug',
         "--configuration={$this->fixture->getPath('docroot/core/phpunit.xml')}",
         '--exclude-group=orca_ignore',
         '--testsuite=orca',
